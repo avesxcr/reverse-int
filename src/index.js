8556dc1ec90cbafe26aa72514a842953f460f0ac
Sevastsyan <avesxcr@gmail.com>
2022-07-04 00:19:39 +0300

    reverse int done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let str = String(n)
+    let newStr = String(n*-1);
+    return n > 0 ? Number(str.split("").reverse().join("")) : Number(newStr.split("").reverse().join(""));
 }
